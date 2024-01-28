4a336d3d729b25661ce08737790b5d7eb7b7f62a
Erick Cordeiro <erick.arruda.ext@infracommerce.com.br>
2024-01-28 08:41:37 -0300

    Criando tela de login

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -11,19 +11,42 @@ class _LoginPageState extends State<LoginPage> {
   String emailField = '';
   String passwordField = '';
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextField(
+  Widget _body() {
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 200,
+                child: Image.asset('assets/images/logo.png'),
+              ),
+              Container(height: 14),
+              Text(
+                'Embalagens Retornáveis',
+                style: TextStyle(
+                  color: Colors.grey[800],
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28,
+                ),
+              ),
+              Text(
+                'Por favor, realize o login para continuar',
+                style: TextStyle(
+                  color: Colors.grey[600],
+                  fontWeight: FontWeight.normal,
+                  fontSize: 16,
+                ),
+              ),
+              Container(height: 14),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 50,
+                child: TextField(
                   onChanged: (value) {
                     emailField = value;
                   },
@@ -33,8 +56,12 @@ class _LoginPageState extends State<LoginPage> {
                     border: OutlineInputBorder(),
                   ),
                 ),
-                const SizedBox(height: 14),
-                TextField(
+              ),
+              const SizedBox(height: 14),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 50,
+                child: TextField(
                   onChanged: (value) {
                     passwordField = value;
                   },
@@ -44,17 +71,21 @@ class _LoginPageState extends State<LoginPage> {
                     border: OutlineInputBorder(),
                   ),
                 ),
-                const SizedBox(height: 10),
-                ElevatedButton(
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 50,
+                child: ElevatedButton(
                   onPressed: () {
                     if (emailField == 'erickcordeiroa@gmail.com' &&
                         passwordField == 'erick2020') {
                       /** 
-                       * The pushName or push in navigation go to
-                       * next page with arrow for before page,
-                       * now pushReplacemant and pushReplacemantName 
-                       * destroy the before page.
-                      **/
+                         * The pushName or push in navigation go to
+                         * next page with arrow for before page,
+                         * now pushReplacemant and pushReplacemantName 
+                         * destroy the before page.
+                        **/
 
                       // ** Way correct do route navegation between pages **
                       Navigator.of(context).pushReplacementNamed('/home');
@@ -65,11 +96,22 @@ class _LoginPageState extends State<LoginPage> {
                     }
                   },
                   child: const Text('Entrar'),
-                )
-              ],
-            ),
+                ),
+              )
+            ],
           ),
         ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          _body(),
+        ],
       ),
     );
   }
